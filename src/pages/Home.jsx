d6b0fd72bd65e476b1db6f809ec220fed0e393ed
Thomas Dami <thomas@damialfredo.com>
2021-07-12 18:12:40 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Content from '../components/Content'
+// import classes from './Home.module.css'
+
+const Home = () => {
+    return (
+        <>
+           <Content></Content>
+        </>
+    )
+}
+
+export default Home;
